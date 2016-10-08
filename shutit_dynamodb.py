f266697b900ca70c3298f13daf2ea395abf29bac
Ian Miell <ian.miell@gmail.com>
2016-10-08 08:07:17 +0100

    ic

---
+++ shutit_dynamodb.py
@@ -0,0 +1,80 @@
+import random
+import string
+
+from shutit_module import ShutItModule
+
+class shutit_dynamodb(ShutItModule):
+
+
+	def build(self, shutit):
+		vagrant_image = shutit.cfg[self.module_id]['vagrant_image']
+		vagrant_provider = shutit.cfg[self.module_id]['vagrant_provider']
+		gui = shutit.cfg[self.module_id]['gui']
+		memory = shutit.cfg[self.module_id]['memory']
+		module_name = 'shutit_dynamodb_' + ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+		shutit.send('rm -rf /tmp/' + module_name + ' && mkdir -p /tmp/' + module_name + ' && cd /tmp/' + module_name)
+		shutit.send('vagrant init ' + vagrant_image)
+		shutit.send_file('/tmp/' + module_name + '/Vagrantfile','''
+Vagrant.configure(2) do |config|
+  config.vm.box = "''' + vagrant_image + '''"
+  # config.vm.box_check_update = false
+  # config.vm.network "forwarded_port", guest: 80, host: 8080
+  # config.vm.network "private_network", ip: "192.168.33.10"
+  # config.vm.network "public_network"
+  # config.vm.synced_folder "../data", "/vagrant_data"
+  config.vm.provider "virtualbox" do |vb|
+    vb.gui = ''' + gui + '''
+    vb.memory = "''' + memory + '''"
+    vb.name = "shutit_dynamodb"
+  end
+end''')
+		shutit.send('vagrant up --provider virtualbox',timeout=99999)
+		shutit.login(command='vagrant ssh')
+		shutit.login(command='sudo su -',password='vagrant')
+
+		shutit.install('openjdk-7-jre')
+		shutit.get_url('dynamodb_local_latest.tar.gz', ['http://dynamodb-local.s3-website-us-west-2.amazonaws.com'])
+		shutit.send('tar -zxvf dynamodb_local_latest.tar.gz')
+		shutit.send('cd dynamodb_local_latest')
+		shutit.send('java -Djava.library.path=./DynamoDBLocal_lib -jar DynamoDBLocal.jar -sharedDb')
+
+		shutit.logout()
+		shutit.logout()
+		return True
+
+	def get_config(self, shutit):
+		shutit.get_config(self.module_id,'vagrant_image',default='ubuntu/precise64')
+		shutit.get_config(self.module_id,'vagrant_provider',default='virtualbox')
+		shutit.get_config(self.module_id,'gui',default='false')
+		shutit.get_config(self.module_id,'memory',default='1024')
+
+		return True
+
+	def test(self, shutit):
+
+		return True
+
+	def finalize(self, shutit):
+
+		return True
+
+	def isinstalled(self, shutit):
+
+		return False
+
+	def start(self, shutit):
+
+		return True
+
+	def stop(self, shutit):
+
+		return True
+
+def module():
+	return shutit_dynamodb(
+		'docker_enterprise_checklist.shutit_dynamodb.shutit_dynamodb', 2052683392.0001,   
+		description='',
+		maintainer='',
+		delivery_methods=['bash'],
+		depends=['shutit.tk.setup','shutit-library.virtualbox.virtualbox.virtualbox','tk.shutit.vagrant.vagrant.vagrant']
+	)
